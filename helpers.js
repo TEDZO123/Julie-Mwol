c43fa350c1fda0a1a8b12c9c232c51b561a61cbb
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2021-12-08 16:03:38 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *PONNU Mwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *PONNU Mwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *PONNU Mwol*:  ```" + msg + "```"
 }
 
 
